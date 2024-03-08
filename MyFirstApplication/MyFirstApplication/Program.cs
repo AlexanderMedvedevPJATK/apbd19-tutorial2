30acdf1b36be8a7acc4b6c1b222f459e3677ba1c
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-03-08 18:51:06 +0100

    Display average on the console

--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -4,3 +4,17 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Mod 1");
 Console.WriteLine("Mod 2");
 Console.WriteLine("Mod 3");
+
+static double GetAverage(int[] numbers)
+{
+    double sum = 0;
+    foreach (var num in numbers)
+    {
+        sum += num;
+    }
+
+    return sum / numbers.Length;
+}
+
+int[] numbers = new[] { 1, 2, 3, 4 };
+Console.WriteLine(GetAverage(numbers));
