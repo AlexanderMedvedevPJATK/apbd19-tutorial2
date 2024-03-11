959ad647031c73322262bb241b7968c04a7feadf
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-03-11 23:44:10 +0100

    minor changes

--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -1,9 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Mod 1");
 Console.WriteLine("Mod 2");
 Console.WriteLine("Mod 3");
+
+var numbers = new[] { 1, 2, 3, 4 };
+Console.WriteLine(GetAverage(numbers));
+
+return;
 
 static double GetAverage(int[] numbers)
 {
@@ -15,6 +18,3 @@ static double GetAverage(int[] numbers)
 
     return sum / numbers.Length;
 }
-
-int[] numbers = new[] { 1, 2, 3, 4 };
-Console.WriteLine(GetAverage(numbers));
