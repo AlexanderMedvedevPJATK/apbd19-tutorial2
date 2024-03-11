08f8081de2d2f0c2b23dca6a8be3e2e74401f192
Alexander Medvedev <s28572@pjwstk.edu.pl>
2024-03-11 23:54:08 +0100

    display max on the console

--- MyFirstApplication/MyFirstApplication/Program.cs
+++ MyFirstApplication/MyFirstApplication/Program.cs
@@ -5,6 +5,7 @@ Console.WriteLine("Mod 3");
 
 var numbers = new[] { 1, 2, 3, 4 };
 Console.WriteLine(GetAverage(numbers));
+Console.WriteLine(GetMax(numbers));
 
 return;
 
@@ -17,4 +18,15 @@ static double GetAverage(int[] numbers)
     }
 
     return sum / numbers.Length;
+}
+
+static double GetMax(int[] numbers)
+{
+    var max = numbers[0];
+    for (int i = 1; i < numbers.Length; i++)
+    {
+        if (numbers[i] > max) max = numbers[i];
+    }
+
+    return max;
 }
